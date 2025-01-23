1b0b4b54ff70fa99eb9653e6df7ddbddfd3a1ffe
sgitwhyd <sigitwahyu.dev@gmail.com>
2025-01-23 16:37:42 +0700

    feat(middleware): create AuthMiddleware & RefreshToken Middleware

---
+++ pkg/router/middleware.go
@@ -0,0 +1,72 @@
+package router
+
+import (
+	"errors"
+	"github.com/gofiber/fiber/v2"
+	"github.com/sgitwhyd/jagong/app/repository"
+	"github.com/sgitwhyd/jagong/pkg/jwt_token"
+	"github.com/sgitwhyd/jagong/pkg/response"
+	"log"
+	"time"
+)
+
+func AuthMiddleware(ctx *fiber.Ctx) error {
+	accessToken := ctx.Get("Authorization")
+	if accessToken == "" {
+		err := errors.New("token is empty").Error()
+		return response.SendErrorResponse(ctx, fiber.StatusUnauthorized, &err)
+	}
+
+	_, err := repository.GetAuthSessionByToken(ctx.Context(), accessToken)
+	if err != nil {
+		log.Printf("get auth session err:%v", err)
+		err := errors.New("token is invalid").Error()
+		return response.SendErrorResponse(ctx, fiber.StatusUnauthorized, &err)
+	}
+
+	claimsToken, err := jwt_token.ValidateToken(ctx.Context(), accessToken)
+	if err != nil {
+		log.Printf("middleware: token validation error: %v", err)
+		err := errors.New("unauthorized").Error()
+		return response.SendErrorResponse(ctx, fiber.StatusUnauthorized, &err)
+	}
+
+	//check if jwt_token expired time is still active or not
+	if time.Now().Unix() > claimsToken.ExpiresAt.Unix() {
+		err := errors.New("token is expired").Error()
+		return response.SendErrorResponse(ctx, fiber.StatusUnauthorized, &err)
+	}
+
+	//set claim jwt_token data into context
+	ctx.Locals("username", claimsToken.Username)
+	ctx.Locals("full_name", claimsToken.FullName)
+
+	return ctx.Next()
+}
+
+func RefreshTokenMiddleware(ctx *fiber.Ctx) error {
+	refreshToken := ctx.Get("Refresh-Token")
+	if refreshToken == "" {
+		err := errors.New("refresh token is empty").Error()
+		return response.SendErrorResponse(ctx, fiber.StatusUnauthorized, &err)
+	}
+
+	claimsToken, err := jwt_token.ValidateToken(ctx.Context(), refreshToken)
+	if err != nil {
+		log.Printf("middleware: refresh token validation error: %v", err)
+		err := errors.New("unauthorized").Error()
+		return response.SendErrorResponse(ctx, fiber.StatusUnauthorized, &err)
+	}
+
+	//check if jwt_token expired time is still active or not
+	if time.Now().Unix() > claimsToken.ExpiresAt.Unix() {
+		err := errors.New("refresh token is expired").Error()
+		return response.SendErrorResponse(ctx, fiber.StatusUnauthorized, &err)
+	}
+
+	//set claim jwt_token data into context
+	ctx.Locals("username", claimsToken.Username)
+	ctx.Locals("full_name", claimsToken.FullName)
+
+	return ctx.Next()
+}
